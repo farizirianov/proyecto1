3951a1098cd3ff8b671f3b3256ab2d23f6cebb5c
farizirianov <farialis92@gmail.com>
2020-06-20 00:52:25 -0400

    segundo commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,35 @@
 <template>
-  <div class="home">
-    <p>hola</p>
-  </div>
+  <v-container fluid pa-3>
+    <v-row>
+
+      <!------------------------ SCREAM LIST ----------------------->
+      <v-col cols="12" sm="8" order="1" order-sm="1">
+
+          <!------------------------ SCREAM ITEM ----------------------->
+          <v-card class="mb-5" elevation="0">
+            <AppScreamCard></AppScreamCard>
+          </v-card>
+          <!------------------------ END SCREAM ITEM ----------------------->
+
+      </v-col>
+      <v-col cols="12" sm="8" order="1">
+          <v-card class="mb-5" elevation="0">
+          </v-card>
+      </v-col>
+      <!------------------------ END SCREAM LIST ----------------------->
+
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
+// COMPONENTS
+import AppScreamCard from '@/components/Home/AppScreamCard.vue';
 
 export default {
   name: 'Home',
   components: {
+    AppScreamCard
   }
 }
 </script>
